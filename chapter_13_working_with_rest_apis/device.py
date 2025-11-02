1d7e24a99e0ee343651e6853cae8706816e20933
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2025-11-02 05:16:59 +0000

    add_static_route method Added

--- chapter_13_working_with_rest_apis/device.py
+++ chapter_13_working_with_rest_apis/device.py
@@ -20,37 +20,72 @@ class Device:
             verify=False
             )
         return response.json()
-    
+
     def add_loopback_interface(self, ipaddr: str, mask: str, name: str, descr: str):
-            payload = {
-                "Cisco-IOS-XE-native:Loopback": [
-                    {
-                        "name": int(name),
-                        "description": f"{descr}",
-                        "ip": {
-                            "address": {
-                                "primary": {
-                                    "address": ipaddr,
-                                    "mask": mask
-                                    }
+        payload = {
+            "Cisco-IOS-XE-native:Loopback": [
+                {
+                    "name": int(name),
+                    "description": f"{descr}",
+                    "ip": {
+                        "address": {
+                            "primary": {
+                                "address": ipaddr,
+                                "mask": mask
                                 }
                             }
                         }
-                    ]
-                }
-            response = requests.post(
-                url=f"{self.base_url}/Cisco-IOS-XE-native:native/interface",
-                json=payload,
-                headers=self.headers,
-                auth=HTTPBasicAuth(username=self.username, password=self.password),
-                verify=False
-                )
-            return response
+                    }
+                ]
+            }
+        response = requests.post(
+            url=f"{self.base_url}/Cisco-IOS-XE-native:native/interface",
+            json=payload,
+            headers=self.headers,
+            auth=HTTPBasicAuth(username=self.username, password=self.password),
+            verify=False
+            )
+        return response
+
+    def get_static_routes_json(self):
+        response = requests.get(
+            url=f"{self.base_url}/Cisco-IOS-XE-native:native/ip/route",
+            headers=self.headers,
+            auth=HTTPBasicAuth(username=self.username, password=self.password),
+            verify=False
+            )
+        return response.json()
+
+    def add_static_route(self, destination: str, mask: str, next_hop: str):
+
+        url = f"{self.base_url}/Cisco-IOS-XE-native:native/ip/route/ip-route-interface-forwarding-list={destination},{mask}"
+
+        payload = {
+            "Cisco-IOS-XE-native:ip-route-interface-forwarding-list": [{
+                "prefix": f"{destination}",
+                "mask": f"{mask}",
+                "fwd-list":
+                    [{"fwd": f"{next_hop}"}
+                     ]
+
+                }]
+            }
+        response = requests.put(
+            url=url,
+            json=payload,
+            headers=self.headers,
+            auth=HTTPBasicAuth(self.username, self.password),
+            verify=False
+        )
+
+        return response
+
 
 
 def main():
     device = Device("10.10.99.1", "script", "cisco123")
-    print(device.get_interfaces_json())
+    result = device.add_static_route("200.0.0.2", "255.255.255.255", "null0")
+    print(result)
 
 if __name__ == "__main__":
     main()
