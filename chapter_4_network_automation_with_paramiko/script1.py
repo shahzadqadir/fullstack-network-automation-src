933641c585a4efa30162b14d0a5f52140adc771b
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2025-11-03 13:36:10 +0000

    created chapter 4 folder and added script1.py to it

---
+++ chapter_4_network_automation_with_paramiko/script1.py
@@ -0,0 +1,13 @@
+# Chapter 4: Network Automation with Paramiko
+# Full Stack Network Automation by Shahzad Qadir
+
+import paramiko
+
+conn = paramiko.SSHClient()
+conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+conn.connect(hostname="10.10.99.11" ,username="script", password="cisco123", allow_agent=False, look_for_keys=False)
+stdin, stdout, stderr = conn.exec_command("show ip int brief")
+output = stdout.readlines()
+for line in output:
+    if line != "":
+        print(line.strip())
